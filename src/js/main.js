977245cf5169db29133f75e0a448e39c97a909bc
Anatolii Velichko <velichko.vega@gmail.com>
2017-12-21 23:09:12 +0200

    statt new project

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+//= ../../node_modules/slick-carousel/slick/slick.js
+
+//= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
