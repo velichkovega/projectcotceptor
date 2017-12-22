9f2c21b5de9a069dc5a3498b990fbba8e7cf8667
Anatolii Velichko <velichko.vega@gmail.com>
2017-12-22 21:33:01 +0200

    add content in tab 1, 1.1, 1.2, 1.3,

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('js:build', function () {
     gulp.src(path.src.js) //Найдем наш main файл
         .pipe(rigger()) //Прогоним через rigger
         .pipe(sourcemaps.init()) //Инициализируем sourcemap
-        .pipe(uglify()) //Сожмем наш js
+        // .pipe(uglify()) //Сожмем наш js
         .pipe(sourcemaps.write()) //Пропишем карты
         .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
         .pipe(reload({stream: true})); //И перезагрузим сервер
@@ -71,7 +71,7 @@ gulp.task('style:build', function () {
         .pipe(sourcemaps.init()) //То же самое что и с js
         .pipe(sass()) //Скомпилируем
         .pipe(prefixer()) //Добавим вендорные префиксы
-        .pipe(cssmin()) //Сожмем
+        //.pipe(cssmin()) //Сожмем
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.build.css)) //И в build
         .pipe(reload({stream: true}));
--- src/js/main.js
+++ src/js/main.js
@@ -7,26 +7,61 @@
 
 //= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
 
-(function($) {
-$(function() {
+//= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
 
-	$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-		$(this)
-			.addClass('active').siblings().removeClass('active')
-			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-	});
 
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+	$(this)
+		.addClass('active').siblings().removeClass('active')
+		.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
-})(jQuery);
 
-(function($) {
-$(function() {
-
-	$('ul.tabs__caption-top').on('click', 'li:not(.active)', function() {
-		$(this)
-			.addClass('active').siblings().removeClass('active')
-			.closest('div.tabs-top').find('div.tabs__content-top').removeClass('active').eq($(this).index()).addClass('active');
-	});
-
+$('ul.tabs__caption-top').on('click', 'li:not(.active)', function () {
+	$(this)
+		.addClass('active').siblings().removeClass('active')
+		.closest('div.tabs-top').find('div.tabs__content-top').removeClass('active').eq($(this).index()).addClass('active');
 });
-})(jQuery);
+
+$('.tabs__content-top--slider').click(() => {
+	setTimeout(() => {
+		$('.main-content__tab-slider').addClass('active');
+
+		$('.main-content__tab-slider').slick({
+			dots: false,
+			infinite: true,
+			speed: 300,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			draggable: true,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						infinite: true,
+						dots: true
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+				// You can unslick at a given breakpoint now by adding:
+				// settings: "unslick"
+				// instead of a settings object
+			]
+		});
+	},0)
+
+})
