5994f32a99bdd62694c70bd134ddb8c44b10fbad
Anatolii Velichko <velichko.vega@gmail.com>
2017-12-22 12:30:58 +0200

    Add basical tabs

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,15 @@
 //= ../../node_modules/slick-carousel/slick/slick.js
 
 //= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
+
+(function($) {
+$(function() {
+
+	$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+		$(this)
+			.addClass('active').siblings().removeClass('active')
+			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+	});
+
+});
+})(jQuery);
